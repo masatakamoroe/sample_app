4abba1682cc70313da7e6b98aac3845cace5f804
masataka moroe <masaozozoz91@gmail.com>
2020-03-05 19:45:48 +0900

    Add account activation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,15 +7,21 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
+    if @user.activated?
       log_in @user
      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
      redirect_back_or @user
     else
-       flash.now[:danger] = 'Invalid email/password combination' # 本当は正しくない
-      # エラーメッセージを作成する
+      message  = "Account not activated. "
+      message += "Check your email for the activation link."
+      flash[:warning] = message
+      redirect_to root_url
+    end
+    else
+    flash.now[:danger] = 'Invalid email/password combination'
     render 'new'
+    end
   end
-end
   def destroy
     log_out if logged_in?
     redirect_to root_url
