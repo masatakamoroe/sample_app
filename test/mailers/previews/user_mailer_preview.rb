557ce709bec5eb36a7c4dafa520e51c1cdeb3eb6
masataka moroe <masaozozoz91@gmail.com>
2020-03-08 10:00:30 +0900

    Add password reset

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -4,6 +4,7 @@ class UserMailerPreview < ActionMailer::Preview
   # Preview this email at
   # http://localhost:3000/rails/mailers/user_mailer/account_activation
   def account_activation
+    user = User.first
     user.activation_token = User.new_token
     UserMailer.account_activation(user)
   end
@@ -11,6 +12,8 @@ class UserMailerPreview < ActionMailer::Preview
   # Preview this email at
   # http://localhost:3000/rails/mailers/user_mailer/password_reset
   def password_reset
-    UserMailer.password_reset
+    user = User.first
+    user.reset_token = User.new_token
+    UserMailer.password_reset(user)
   end
 end
