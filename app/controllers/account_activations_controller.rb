bd5990825a252a8e086a384e831c300f093f0970
masataka moroe <masaozozoz91@gmail.com>
2020-03-21 10:58:15 +0900

    Add password resets & email configuration

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,17 +1,16 @@
 class AccountActivationsController < ApplicationController
-
+  # メールクリック、アカウント有効化
   def edit
-    user = User.find_by(email: params[:email])
-    if user && !user.activated? && user.authenticated?(:activation, params[:id])
-      #user.activate
-      user.update_attribute(:activated,     true)
-      user.update_attribute(:activated_at,  Time.zone.now)
+    user = User.find_by(email:params[:email])
+    if user && !user.activated? && user.authenticated?(:activation,params[:id])
+      user.update_attribute(:activated,true)
+      user.update_attribute(:activated_at,Time.zone.now)
       log_in user
-      flash[:success] = "Account activated!"
+      flash[:success] = "Account activated"
       redirect_to user
     else
-      flash[:danger] = "Invalid activation link"
+      flash[:danger] = "Invalid activation link."
       redirect_to root_url
     end
   end
-end
+end 
--- db/migrate/20200303015510_add_admin_to_users.rb
+++ db/migrate/20200303015510_add_admin_to_users.rb
@@ -2,7 +2,7 @@ class AddAdminToUsers < ActiveRecord::Migration[5.1]
   def change
     add_column :users, :admin, :boolean,default: false
     add_column :users, :activation_digest, :string
-   add_column :users, :activated, :boolean, default: false
-   add_column :users, :activated_at, :datetime
+    add_column :users, :activated, :boolean, default: false
+    add_column :users, :activated_at, :datetime
   end
 end
