70562b2a0f556381e8b70781e11b715cf67f41f6
masataka moroe <masaozozoz91@gmail.com>
2020-03-25 00:49:20 +0900

    Add user following

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200319111533) do
+ActiveRecord::Schema.define(version: 20200322015308) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 20200319111533) do
     t.index ["user_id"], name: "index_microposts_on_user_id"
   end
 
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -29,6 +39,7 @@ ActiveRecord::Schema.define(version: 20200319111533) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at"
